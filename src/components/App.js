b501d07dfdaace915b40b10a706d0c1b6827a145
07pra <120910332+07pra@users.noreply.github.com>
2023-03-12 14:01:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,55 @@ import '../styles/App.css';
 const App = () => {
 
 //code here 
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  const [formInput, setFormInput] = useState({ email:"", password:"",});
+  const [displayMsg,setDisplayMsg] = useState({emailMsg:"",passwordMsg:"",});
+  const handleInputChange = (e) => {
+   
+         e.persist();
+        // console.log(e.target, e.target.id, e.target.value);
+        if (e.target.id === "inputEmail") {
+            setFormInput((state) => {
+                return {
+                    ...state,
+                    ["email"]: e.target.value,
+                };
+            });
+        } else {
+           setFormInput((state) => {
+                return {
+                    ...state,
+                    ["password"]: e.target.value,
+                };
+            });
+        }
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formInput.email === "") {
+            emailRef.current.focus();
+            setDisplayMsg({
+                emailMsg: "",
+                passwordMsg: "",
+            });
+        } else if (formInput.password === "") {
+            passwordRef.current.focus();
+            setDisplayMsg({
+                emailMsg: "",
+                passwordMsg: "",
+            });
+        } else {
+            setDisplayMsg({
+                emailMsg: formInput.email,
+                passwordMsg: formInput.password,
+            });
+            setFormInput({
+                email: "",
+                password: "",
+            });
+        }
+    };
 
   return (
     <div id="main">
